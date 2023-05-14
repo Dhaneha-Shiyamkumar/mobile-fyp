7ec4588e13b04b6a8ca9cefc70a1d35052978ba0
z9fr <z9fr@protonmail.com>
2023-05-15 01:21:06 +0530

    feat: child details re-wamp

--- screens/child-details.tsx
+++ screens/child-details.tsx
@@ -1,142 +1,208 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import {Avatar, Card, Text} from 'react-native-paper';
-import {Dimensions, ScrollView} from 'react-native';
+import {Appbar, Avatar, Card, Text, Title} from 'react-native-paper';
+import {Dimensions, ScrollView, StyleSheet, View} from 'react-native';
 import {IChild} from '../types/user-children';
 import {LineChart} from 'react-native-chart-kit';
-
-const LeftContent = (props: {firstName: string; lastName: string}) => {
-  return (
-    <Avatar.Text
-      size={40}
-      label={`${props.firstName.charAt(0)}${props.lastName.charAt(0)}`}
-    />
-  );
-};
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export const ChildDeatils = ({route, navigation}: any) => {
   const {child} = route.params as {child: IChild};
   return (
-    <ScrollView style={{padding: 10}}>
-      <Text variant="titleLarge" style={{fontWeight: 'bold'}}>
-        Details about {child.firstName}
-      </Text>
+    <SafeAreaView style={styles.container}>
+      <Appbar.Header style={styles.header}>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Title style={styles.conversationsTitle}>about {child.firstName}</Title>
+      </Appbar.Header>
 
-      <Card style={{marginTop: 20}} mode="outlined">
-        <Card.Title
-          title={`${child.firstName} ${child.lastName}`}
-          left={() => {
-            return (
-              <LeftContent
-                firstName={child.firstName}
-                lastName={child.lastName}
-              />
-            );
+      <ScrollView
+        style={{
+          padding: 20,
+        }}>
+        <View style={styles.profileHeader}>
+          <Avatar.Text
+            label={`${child.firstName[0]}${child.lastName[0]}`}
+            size={64}
+          />
+          <Text
+            style={styles.name}>{`${child.firstName} ${child.lastName}`}</Text>
+        </View>
+
+        <Card
+          style={{
+            paddingLeft: 10,
+            paddingRight: 10,
+            backgroundColor: '#E8F3F1',
+            elevation: 0,
+          }}>
+          <Card.Content>
+            <Text variant="bodyMedium">
+              {`here you can find the Information about ${child.firstName}, the uptodate information added by the nurse `}
+            </Text>
+          </Card.Content>
+        </Card>
+
+        <Text
+          variant="headlineSmall"
+          style={{fontWeight: 'bold', marginTop: 20}}>
+          Child Height Information
+        </Text>
+
+        <LineChart
+          data={{
+            labels: [
+              'Jan',
+              'Feb',
+              'Mar',
+              'Apr',
+              'May',
+              'June',
+              'July',
+              'Aug',
+              'Sept',
+              'Oct',
+              'Nov',
+              'Dec',
+            ],
+            datasets: [
+              {
+                data: child.attributes.height.map(
+                  ({value}: {value: number}) => value,
+                ),
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width} // from react-native
+          height={220}
+          yAxisInterval={1}
+          chartConfig={{
+            backgroundColor: '#ffffff',
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          }}
+          bezier
+          style={{
+            marginTop: 20,
+            borderRadius: 16,
           }}
         />
-        <Card.Content>
-          <Text variant="bodyMedium">
-            {`here you can find the Information about ${child.firstName}, the uptodate information added by the nurse `}
-          </Text>
-        </Card.Content>
-        <Card.Cover
-          source={{uri: 'https://picsum.photos/700'}}
-          style={{padding: 10, backgroundColor: 'white', marginBottom: 10}}
+
+        <Text
+          variant="headlineSmall"
+          style={{fontWeight: 'bold', marginTop: 20}}>
+          Child Weight Information
+        </Text>
+
+        <LineChart
+          data={{
+            labels: [
+              'Jan',
+              'Feb',
+              'Mar',
+              'Apr',
+              'May',
+              'June',
+              'July',
+              'Aug',
+              'Sept',
+              'Oct',
+              'Nov',
+              'Dec',
+            ],
+            datasets: [
+              {
+                data: child.attributes.weight.map(
+                  ({value}: {value: number}) => value,
+                ),
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width} // from react-native
+          height={220}
+          yAxisInterval={1}
+          chartConfig={{
+            backgroundColor: '#ffffff',
+            backgroundGradientFrom: '#ffffff',
+            backgroundGradientTo: '#ffffff',
+            decimalPlaces: 0,
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          }}
+          bezier
+          style={{
+            marginTop: 20,
+            borderRadius: 16,
+          }}
         />
-      </Card>
-
-      <Text variant="headlineSmall" style={{fontWeight: 'bold', marginTop: 20}}>
-        Child Height Information
-      </Text>
-
-      <LineChart
-        data={{
-          labels: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'June',
-            'July',
-            'Aug',
-            'Sept',
-            'Oct',
-            'Nov',
-            'Dec',
-          ],
-          datasets: [
-            {
-              data: child.attributes.height.map(
-                ({value}: {value: number}) => value,
-              ),
-            },
-          ],
-        }}
-        width={Dimensions.get('window').width} // from react-native
-        height={220}
-        yAxisInterval={1}
-        chartConfig={{
-          backgroundColor: '#ffffff',
-          backgroundGradientFrom: '#ffffff',
-          backgroundGradientTo: '#ffffff',
-          decimalPlaces: 0,
-          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-        }}
-        bezier
-        style={{
-          marginTop: 20,
-          borderRadius: 16,
-        }}
-      />
-
-      <Text variant="headlineSmall" style={{fontWeight: 'bold', marginTop: 20}}>
-        Child Weight Information
-      </Text>
-
-      <LineChart
-        data={{
-          labels: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'June',
-            'July',
-            'Aug',
-            'Sept',
-            'Oct',
-            'Nov',
-            'Dec',
-          ],
-          datasets: [
-            {
-              data: child.attributes.weight.map(
-                ({value}: {value: number}) => value,
-              ),
-            },
-          ],
-        }}
-        width={Dimensions.get('window').width} // from react-native
-        height={220}
-        yAxisInterval={1}
-        chartConfig={{
-          backgroundColor: '#ffffff',
-          backgroundGradientFrom: '#ffffff',
-          backgroundGradientTo: '#ffffff',
-          decimalPlaces: 0,
-          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-        }}
-        bezier
-        style={{
-          marginTop: 20,
-          borderRadius: 16,
-        }}
-      />
-
+      </ScrollView>
       <Text style={{marginBottom: 20}}>{''}</Text>
-    </ScrollView>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  conversations: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+    elevation: 3,
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    paddingLeft: 20,
+  },
+  header: {
+    backgroundColor: '#fff',
+    elevation: 3,
+  },
+  conversationsTitle: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  profileContainer: {
+    flex: 1,
+    padding: 16,
+  },
+  profileHeader: {
+    alignItems: 'center',
+    marginBottom: 24,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 16,
+  },
+  form: {
+    flex: 1,
+  },
+  spacer: {
+    height: 16,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  option: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8,
+  },
+  optionLabel: {
+    fontSize: 16,
+  },
+});
--- screens/child-list.tsx
+++ screens/child-list.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {useUserChildrens} from '../hooks/users/use-children';
 import {
   ActivityIndicator,
@@ -8,53 +10,120 @@ import {
   Avatar,
   Divider,
   List,
+  Title,
+  IconButton,
+  Caption,
+  Appbar,
 } from 'react-native-paper';
 import {UserChildren} from '../types/user-children';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {RefreshControl, ScrollView} from 'react-native-gesture-handler';
 
 export function HomeScreen({route, navigation}: any) {
   const {userId} = route.params as {userId: string};
 
-  const {data, isLoading} = useUserChildrens(userId) as {
+  const {data, isLoading, refetch} = useUserChildrens(userId) as {
     data: UserChildren;
     isLoading: boolean;
+    refetch: () => void;
   };
 
-  return (
-    <View style={{padding: 10}}>
-      {isLoading && (
-        <ActivityIndicator animating={true} color={MD2Colors.red800} />
-      )}
+  const [refreshing, setRefreshing] = React.useState(false);
 
-      {data && (
-        <>
-          {data.map(child => {
-            return (
-              <>
-                <List.Item
-                  key={child._id}
-                  title={`${child.firstName} ${child.lastName}`}
-                  description={`Click here to view details about ${child.firstName}`}
-                  onPress={() =>
-                    navigation.navigate('Details', {
-                      child: child,
-                    })
-                  }
-                  left={_props => (
-                    <Avatar.Text
-                      key={child._id}
-                      size={40}
-                      label={`${child.firstName.charAt(
-                        0,
-                      )}${child.lastName.charAt(0)}`}
-                    />
-                  )}
-                />
-                <Divider />
-              </>
-            );
-          })}
-        </>
-      )}
-    </View>
+  const onRefresh = React.useCallback(() => {
+    const refetchData = async () => {
+      refetch();
+      setRefreshing(false);
+    };
+
+    setRefreshing(true);
+    refetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Appbar.Header style={styles.header}>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+        <Title style={styles.conversationsTitle}>Children Details</Title>
+      </Appbar.Header>
+
+      <ScrollView
+        style={{height: '100%'}}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        {isLoading && (
+          <ActivityIndicator animating={true} color={MD2Colors.red800} />
+        )}
+
+        <List.Section
+          style={{
+            marginLeft: 10,
+            marginRight: 10,
+          }}>
+          {data && (
+            <>
+              {data.map(child => (
+                <>
+                  <List.Item
+                    key={child._id}
+                    title={child.firstName + ' ' + child.lastName}
+                    description={`Click here to view details about ${child.firstName}`}
+                    left={() => (
+                      <>
+                        <Avatar.Text
+                          size={40}
+                          key={child._id}
+                          label={child.firstName[0] + child.lastName[0]}
+                        />
+                      </>
+                    )}
+                    onPress={() =>
+                      navigation.navigate('Details', {
+                        child: child,
+                      })
+                    }
+                  />
+                  <Divider />
+                </>
+              ))}
+            </>
+          )}
+        </List.Section>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  conversations: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+    elevation: 3,
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    paddingLeft: 20,
+  },
+  header: {
+    backgroundColor: '#fff',
+    elevation: 3,
+  },
+  conversationsTitle: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+});
